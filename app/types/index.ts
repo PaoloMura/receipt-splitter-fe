5e35f75625313e4dd24b28b3b76fbae0d27e960b
Tanya Waqanika <twaqanika31@gmail.com>
2025-04-16 17:22:44 +0100

    Added functionality to edit items page

---
+++ app/types/index.ts
@@ -0,0 +1,5 @@
+export type Item = {
+    name: string;
+    label: string;
+    cost: string;
+  };
--- pages/edit-items.tsx
+++ pages/edit-items.tsx
@@ -1,12 +1,48 @@
 import { ThemedText } from "@/components/ThemedText";
 import { styles } from "@/constants/styles";
 import { View } from "react-native";
+import { InputRow } from "@/components/InputRow";
+import { ScrollView } from "react-native-gesture-handler";
+import { Item } from "@/app/(tabs)";
+import Button from "@/components/Button";
 
-export default function EditItems() {
+type EditItemsProps = {
+  items: Item[];
+  setItems: React.Dispatch<React.SetStateAction<Item[]>>;
+  onNext: () => void;
+};
+
+const EditItems: React.FC<EditItemsProps> = ({ items, setItems, onNext }) => {
+  const updateItemCost = (index: number, newCost: string) => {
+    const updatedItems = [...items];
+    updatedItems[index].cost = newCost;
+    setItems(updatedItems);
+  };
+
+  const removeItem = (index: number) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <ThemedText type="title">Edit items</ThemedText>
       <ThemedText type="default">Make any corrections if necessary</ThemedText>
-    </View>
+
+      {items.map((item, index) => (
+        <InputRow
+          key={index}
+          label={item.label}
+          value={item.cost}
+          setValue={(val) => updateItemCost(index, val)}
+          onRemove={() => removeItem(index)}
+        />
+      ))}
+
+       <Button onPress={onNext} label="Next" />
+    </ScrollView>
   );
-}
+};
+
+export default EditItems;
